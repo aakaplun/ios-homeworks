e51ea1142909f9f579f77788ad50687d2bc00a08
Andrey Kaplun <kaplun@me.com>
2022-02-11 20:01:07 +0300

    initial commit

---
+++ Navigation/Navigation/PostViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PostViewController.swift
+//  Navigation
+//
+//  Created by Andrey Kaplun on 10/02/2022.
+//  Copyright © 2022 Andrey Kaplun. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Navigation/Navigation/TabViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TabViewController.swift
+//  Navigation
+//
+//  Created by Andrey Kaplun on 10/02/2022.
+//  Copyright © 2022 Andrey Kaplun. All rights reserved.
+//
+
+import UIKit
+
+class TabViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
